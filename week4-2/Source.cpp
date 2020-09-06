c1034744aefa55b7395db9e5e244b590722bf925
KRITSANAPHAT PHANJAROEN <63010040@KMITL.AC.TH>
2020-09-06 17:30:42 +0700

    week4-2

---
+++ week4-2/Source.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    char a = 'A';
+    int n, x;
+    int c = 1;
+
+    printf("Input number  : ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (x = 1; x <= n - i; x++)
+
+            printf("  ");
+        for (j = 0; j <= (c / 2); j++) {
+            printf("%c ", a++);
+        }
+
+        a = a - 2;
+
+        for (j = 0; j < (c / 2); j++) {
+            printf("%c ", a--);
+        }
+        c = c + 2;
+        a = 'A';
+        printf("\n");
+    }
+}
